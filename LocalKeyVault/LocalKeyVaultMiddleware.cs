4c4bec375d4fc2cd4e6cd5eec9d592ac61914e3b
Sam Williamson <samuel.g.williamson@gmail.com>
2019-07-31 12:19:20 -0400

    Initial spike

---
+++ LocalKeyVault/LocalKeyVaultMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalKeyVault
+{
+    public class LocalKeyVaultMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _config;
+
+        public LocalKeyVaultMiddleware(RequestDelegate next, IConfiguration config)
+        {
+            _next = next;
+            _config = config;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.HasValue && context.Request.Path.Value.Contains("/secrets"))
+            {
+                string key = context.Request.Path.Value.Replace("/", "").Replace("secrets", "");
+                if (!string.IsNullOrEmpty(_config.GetValue<string>($"KeyVaultValues:{key}")))
+                {
+                    var bundle = new SecretBundle(value: _config.GetValue<string>($"KeyVaultValues:{key}"));
+
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(bundle));
+                }
+            } 
+
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class LocalKeyVaultMiddlewareExtension
+    {
+        public static IApplicationBuilder UseLocalKeyVault(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<LocalKeyVaultMiddleware>();
+
+            return app;
+        }
+    }
+}
---
+++ LocalKeyVault/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace LocalKeyVault
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        private readonly IHostingEnvironment _env;
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Use(async (HttpContext context, Func<Task> next) =>
+            //{
+            //    //do work before the invoking the rest of the pipeline
+
+            //    if (context.Request.Path.HasValue && context.Request.Path.Value.Contains("/secrets"))
+            //    {
+            //        string key = context.Request.Path.Value.Replace("/", "").Replace("secrets", "");
+            //        if (!string.IsNullOrEmpty(Configuration.GetValue<string>($"KeyVaultValues:{key}")))
+            //        {
+            //            var bundle = new SecretBundle()
+            //            {
+            //                Value = Configuration.GetValue<string>($"KeyVaultValues:{key}"),
+            //                Id = key
+            //            };
+
+            //            await context.Response.WriteAsync(JsonConvert.SerializeObject(bundle));
+            //        }
+            //    }
+
+            //    await next.Invoke(); //let the rest of the pipeline run
+
+            //    //do work after the rest of the pipeline has run     
+            //});
+            app.UseLocalKeyVault();
+        }
+    }
+}
